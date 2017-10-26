0a030f44e5fe8bf5b202083f0aed9f6a7db39b0e
Alexander <alexbrianzhang@gmail.com>
2017-10-25 21:55:39 -0400

    Tried to add redis conversation caching

--- app.py
+++ app.py
@@ -1,59 +1,67 @@
-import os, sys, json, random
-import requests
-import numpy as np
-from numpy.random import choice
+import os, sys, json, random, requests
 from dotenv import load_dotenv
 from flask import Flask, request
+from flask.ext.cacheify import init_cacheify
+
+import numpy as np
 from utils import ModelLoader
 
-app = Flask(__name__)
-
+# load config vars, init app and cache
 load_dotenv(".env")
+app = Flask(__name__)
+cache = init_cacheify(app)
+
+# load keras model
 modelLoader = ModelLoader("model.json", "weights.hdf5")
 modelLoader.start()
 chars = ['\n', ' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '>', '?', '[', ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\x92', '\xa0', '¡', '¿', 'à', 'á', 'ä', 'è', 'é', 'ê', 'í', 'ñ', 'ó', 'ö', 'ú']
 char_indices = dict((c, i) for i, c in enumerate(chars))
 
+
 @app.route('/', methods=['GET'])
 def verify():
-    # when the endpoint is registered as a webhook, it must echo back
-    # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
-
-    return "I would just like to let everyone know that I suck.", 200
+    return "Improper verification request", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
-    # endpoint for processing incoming messaging events
     data = request.get_json()
-    print(data)  # you may not want to log every incoming message in production, but it's good for testing
+    for sender, message in messaging_events(payload):
+        #print("Incoming from {sender}: {text}".format(sender=sender, message=message)) # for testing only
+        messages = app.cache.get(sender).append(message) # get message history from cache
+        dialogue = '\n'.join(
+            ['simmons:'+messages[i] if i % 2 == 0 else 'grif:'+messages[i] for i in range(len(messages))])
+        print(dialogue)
 
-    if data["object"] == "page":
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-                if messaging_event.get("message"):  # someone sent us a message
-                    sender_id = messaging_event["sender"]["id"] # the facebook ID of the person sending you the message
-                    try:
-                        message_text = messaging_event["message"]["text"]  # the message's text
-                    except KeyError:
-                        message_text = ""
-                    send_message(sender_id, get_dialogue(message_text))
+        response = get_dialogue(dialogue) 
+        app.cache.set(sender, messages.append(response)[-8:], 180) # cache last 8 messages
+        send_message(sender, response)
+    return "ok"
 
-    return "ok", 200
+
+def messaging_events(payload):
+    data = json.loads(payload)
+    events = data["entry"][0]["messaging"]
+    for event in events:
+        if "message" in event and "text" in event["message"]:
+            yield event["sender"]["id"], event["message"]["text"].lower()
+        else:
+            yield event["sender"]["id"], "i have no idea."
+
 
 def get_dialogue(message_text, temp=0.65, maxlen=250):
-    if modelLoader.getModel() is None:
-        return "grifbot is loading"
     model = modelLoader.getModel()
-    starter_lines = modelLoader.getStarterLines()
+    if model is None:
+        return "grifbot is loading"
 
+    starter_lines = modelLoader.getStarterLines()
     random.shuffle(starter_lines)
     starter = "\n".join(starter_lines)
-    seed_string = starter + "\n simmons:" + message_text.lower() + "\n grif:"
+    seed_string = starter + message_text + "\n grif:"
     startlen = len(seed_string)
 
     with modelLoader.getGraph().as_default():
@@ -65,32 +73,31 @@ def get_dialogue(message_text, temp=0.65, maxlen=250):
             preds = np.log(preds) / temp
             exp_preds = np.exp(preds)
             preds = exp_preds / np.sum(exp_preds)
-            next_char = choice(chars, p=preds)
+            next_char = np.random.choice(chars, p=preds)
             seed_string = seed_string + next_char
     
     return seed_string[startlen:-1]
 
-def send_message(recipient_id, message_text):
 
-    print("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
+def send_message(recipient, message):
+    #print("Sending to {recipient}: {message}".format(recipient=recipient, message=message)) # for testing only
     params = { "access_token": os.environ["PAGE_ACCESS_TOKEN"] }
     headers = { "Content-Type": "application/json" }
     data = json.dumps({
         "recipient": {
-            "id": recipient_id
+            "id": recipient
         },
         "message": {
-            "text": message_text
+            "text": message
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
+        print(r.text)
+
 
 if __name__ == '__main__':
-    while True:
+    while True: # for testing conversations offline
         ins = input('Say something: ')
         print(get_dialogue(ins))
     # app.run(debug=True)
