50a7cc78373fcff417c776019a8bf0abee147956
Alexander <alexbrianzhang@gmail.com>
2018-11-12 12:45:33 -0500

    Change to Pipenv

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
 from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 REDIS_URL = os.environ.get('REDIS_URL')
