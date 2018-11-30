634c15a4d627773539035c7faa93f37087e0d4fe
U-DESKTOP-8A10LVA\Alex_Zhang <alexbrianzhang@gmail.com>
2018-11-29 21:24:38 -0500

    Add textgenrnn model

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from textgenrnn import textgenrnn
 
-textgen = textgenrnn()
-textgen.train_from_file('script.txt', num_epochs=20000)
+textgen = textgenrnn(name="textgenrnn_model")
+textgen.train_from_file('script.txt', num_epochs=100, new_model=True, word_level=True, rnn_bidirectional=True, dim_embeddings=300)
 textgen.generate(100, temperature=1.0)
